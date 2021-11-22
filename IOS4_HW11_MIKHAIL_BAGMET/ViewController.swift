683b4f15f3790822bb77840e8e16799d017f3d06
Mikhail Bagmet <m.bagmet@gmail.com>
2021-11-22 20:19:03 +0200

    Add parentStackView and timer label.

--- IOS4_HW11_MIKHAIL_BAGMET/ViewController.swift
+++ IOS4_HW11_MIKHAIL_BAGMET/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var parentStackView: UIStackView = {
+        let stackView = UIStackView()
+
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 100
+
+        return stackView
+    }()
+
+    private lazy var timerLabel: UILabel = {
+        let label = UILabel()
+
+        label.text = "25:00"
+        label.font = .systemFont(ofSize: 70, weight: .thin)
+        label.textColor = #colorLiteral(red: 0.9921568627, green: 0.5529411765, blue: 0.5137254902, alpha: 1)
+        label.adjustsFontSizeToFitWidth = true
+
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupHierarchy()
+        setupLayout()
+        setupView()
     }
 
+    // MARK: - Settings
+    private func setupHierarchy() {
+        view.addSubview(parentStackView)
 
+        parentStackView.addArrangedSubview(timerLabel)
+    }
+
+    private func setupLayout() {
+        parentStackView.translatesAutoresizingMaskIntoConstraints = false
+        parentStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        parentStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120).isActive = true
+        parentStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        parentStackView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+    }
+
+    private func setupView() {
+
+    }
 }
 
