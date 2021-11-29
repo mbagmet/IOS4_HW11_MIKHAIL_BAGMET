4c6986ed90efe20eac4f790b0c66f11ccff7f456
Mikhail Bagmet <m.bagmet@gmail.com>
2021-11-29 22:21:23 +0200

    Add static TimerLabel.

--- IOS4_HW11_MIKHAIL_BAGMET/timer.swift
+++ IOS4_HW11_MIKHAIL_BAGMET/timer.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 protocol Stopwatch {
-    var isWorkTime: Bool { get set }
+    static var isWorkTime: Bool { get set }
     var isStarted: Bool { get set }
     var isPaused: Bool { get set }
     var timer: Timer? { get set }
@@ -17,12 +17,12 @@ protocol Stopwatch {
 
     func startTimer()
     func pauseTimer()
-    func updateTimer()
+    //func updateTimer()
     func convertSecondsToString(timeLeft: TimeInterval) -> String
 }
 
 class PomodoroTimer: Stopwatch {
-    var isWorkTime = true
+    static var isWorkTime = true
     var isStarted = false
     var isPaused = false
     var timer: Timer?
@@ -51,15 +51,16 @@ class PomodoroTimer: Stopwatch {
     @objc func updateTimer() {
         timeLeft -= 0.01
         print(timeLeftString)
-        //viewController.timerLabel1.text = "414"
+        ViewController.timerLabel.text = timeLeftString
 
         if timeLeft <= 0 {
+            isStarted = false
             timer?.invalidate()
             timer = nil
-            if isWorkTime {
-                isWorkTime = false
+            if PomodoroTimer.isWorkTime {
+                PomodoroTimer.isWorkTime = false
             } else {
-                isWorkTime = true
+                PomodoroTimer.isWorkTime = true
             }
         }
     }
@@ -73,5 +74,4 @@ class PomodoroTimer: Stopwatch {
         return formatter.string(from: timeLeft) ?? "00:00"
     }
 }
-
 
