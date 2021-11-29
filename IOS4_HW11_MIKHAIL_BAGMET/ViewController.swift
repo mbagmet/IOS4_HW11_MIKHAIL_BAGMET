8f2aacc32f839eee0f26c0f484222ca1a3713ed6
Mikhail Bagmet <m.bagmet@gmail.com>
2021-11-29 23:38:15 +0200

    Add timer layout.

--- IOS4_HW11_MIKHAIL_BAGMET/ViewController.swift
+++ IOS4_HW11_MIKHAIL_BAGMET/ViewController.swift
@@ -15,6 +15,34 @@ class ViewController: UIViewController {
         return progressBar
     }()
 
+    private lazy var parentStackView: UIStackView = {
+        let stackView = UIStackView()
+
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+
+        return stackView
+    }()
+
+    private lazy var timerLabel: UILabel = {
+        var label = UILabel()
+
+        label.text = Strings.workingTimeLeft
+        label.font =  .systemFont(ofSize: Metric.timeLabelFontSize, weight: .thin)
+        label.textColor = Colors.workColor
+        label.adjustsFontSizeToFitWidth = true
+
+        return label
+    }()
+
+    private lazy var playButton: UIButton = {
+        var button = UIButton()
+        button = createButton(with: "play.fill", tintColor: Colors.workColor)
+        button.addTarget(self, action: #selector(playButtonAction), for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,19 +53,77 @@ class ViewController: UIViewController {
     // MARK: - Settings
     private func setupHierarchy() {
         view.addSubview(progressBarView)
+        progressBarView.addSubview(parentStackView)
+        parentStackView.addArrangedSubview(timerLabel)
+        parentStackView.addArrangedSubview(playButton)
     }
 
     private func setupLayout() {
         progressBarView.translatesAutoresizingMaskIntoConstraints = false
-        progressBarView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
-        progressBarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120).isActive = true
-        progressBarView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
-        progressBarView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        progressBarView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Metric.progressBarViewLeading).isActive = true
+        progressBarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Metric.progressBarViewTop).isActive = true
+        progressBarView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: Metric.progressBarViewTrailing).isActive = true
+        progressBarView.heightAnchor.constraint(equalToConstant: Metric.progressBarViewHeight).isActive = true
+
+        parentStackView.translatesAutoresizingMaskIntoConstraints = false
+        parentStackView.leadingAnchor.constraint(equalTo: progressBarView.leadingAnchor, constant: Metric.progressBarViewLeading).isActive = true
+        parentStackView.topAnchor.constraint(equalTo: progressBarView.topAnchor, constant: Metric.parentStackViewTop).isActive = true
+        parentStackView.trailingAnchor.constraint(equalTo: progressBarView.trailingAnchor, constant: Metric.progressBarViewTrailing).isActive = true
+        parentStackView.heightAnchor.constraint(equalToConstant: Metric.parentStackViewHeight).isActive = true
     }
 
     private func setupView() {
 
     }
 
+    // MARK: - Private functions
+    private func createButton(with icon: String, tintColor: UIColor) -> UIButton {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .white
+        setImageForButton(icon: icon, button: button)
+        button.tintColor = tintColor
+
+        button.heightAnchor.constraint(equalTo: button.heightAnchor, multiplier: 1).isActive = true
+        button.widthAnchor.constraint(equalTo: button.heightAnchor, multiplier: 1).isActive = true
+
+        return button
+    }
+
+    private func setImageForButton(icon: String, button: UIButton) {
+        button.setImage(UIImage(systemName: icon)?.applyingSymbolConfiguration(UIImage
+            .SymbolConfiguration(font: .systemFont(ofSize: Metric.buttonImageSize, weight: .thin))), for: .normal)
+    }
+
+    // MARK: - Actions
+    @objc private func playButtonAction() {
+
+    }
+}
+
+// MARK: - Constants
+extension ViewController {
+
+    enum Metric {
+        static let buttonHeight: CGFloat = 50
+        static let buttonImageSize: CGFloat = 36
+        static let timeLabelFontSize: CGFloat = 70
+
+        static let progressBarViewLeading: CGFloat = 20
+        static let progressBarViewTop: CGFloat = 120
+        static let progressBarViewTrailing: CGFloat = -20
+        static let progressBarViewHeight: CGFloat = 300
+
+        static let parentStackViewTop: CGFloat = 83
+        static let parentStackViewHeight: CGFloat = 152
+    }
+
+    enum Strings {
+        static let workingTimeLeft: String = "25:00"
+    }
+
+    enum Colors {
+        static var workColor: UIColor = #colorLiteral(red: 0.9921568627, green: 0.5529411765, blue: 0.5137254902, alpha: 1)
+        static var restColor: UIColor = #colorLiteral(red: 0.3882352941, green: 0.768627451, blue: 0.6431372549, alpha: 1)
+    }
 }
 
